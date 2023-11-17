3a2942473fae12fef55abb7c94a86ec0abbdff0f
Geraldoviananeto <geraldovianarn@gmail.com>
2023-11-17 11:23:41 -0300

    uptade code

--- mudaracesso.php
+++ mudaracesso.php
@@ -6,49 +6,41 @@ include('conexao.php');
 include('validaradmin.php');
 
 
-    $select = "SELECT nome, descricao,nivel.id,login.cpf FROM usuario
+$select = "SELECT nome, descricao,nivel.id,login.cpf FROM usuario
     INNER JOIN login ON usuario.cpf = login.cpf INNER JOIN nivel ON nivel.id = nivel";
-    $queryselect = mysqli_query($conexao, $select);
+$queryselect = mysqli_query($conexao, $select);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <center>
-        <form name="mudatipo" action="mudartipo.php" method="POST">
-            <table border="1px">
+
+        <table border="1px">
+            <tr>
+                <td>Nome</td>
+
+            </tr>
+            <?php
+            while ($linha = mysqli_fetch_row($queryselect)) { ?>
                 <tr>
-                    <td>nome</td>
-                    <td>tipo de usuário</td>
-                    <td>novo tipo de usuário</td>
-                    <td>alterar</td>
-                </tr>
-                <?php
-                while($linha = mysqli_fetch_row($queryselect)) { ?>
-                <tr>
-                    <td><?php echo $linha[0]?></td>
-                    <td><?php echo $linha[1]?></td>
-                    <td>
-                        <select name="nivel">
-                            <option value="1">administrador</option>
-                            <option value="2">gerente</option>
-                            <option value="3">usuário</option>
-                        </select>
-                    </td>
-                <td>
-                <input type="submit" name="alterar" value="alterar">
-                <input type="hidden" name="cpf" value="<?php echo $linha[3]?>">
-                </td>
+                    <td> <a href="chamausuario.php?cod=<?php
+                    echo $linha[3] ?>"><?php
+                    echo $linha[0] ?></a></td>
+
                 <?php } ?>
-                </table>
-                </form>
-                </center>
+        </table>
+
+    </center>
 </body>
+
 </html>
